cf4354516ceeee08a4ef64b9f0b4e2c7a1a5b454
ribeiro-henrique <hesr.ribeiro@gmail.com>
2023-09-25 11:29:49 -0300

    req 1

---
+++ app/backend/src/database/migrations/create-team-table.ts
@@ -0,0 +1,23 @@
+import { DataTypes, Model, QueryInterface } from 'sequelize';
+import ITeam from '../../Interfaces/ITeam';
+
+export default {
+  up(queryInterface: QueryInterface) {
+    return queryInterface.createTable<Model<ITeam>>('teams', {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      teamName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: 'team_name',
+      }
+    });
+  },
+  down(queryInterface: QueryInterface) {
+    return queryInterface.dropTable('teams');
+  },
+};
---
+++ app/backend/src/database/models/Teams.ts
@@ -0,0 +1,40 @@
+import {
+  CreationOptional, DataTypes, InferAttributes,
+  InferCreationAttributes, Model
+} from 'sequelize';
+
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class Team extends Model<InferAttributes<Team>,
+InferCreationAttributes<Team>> {
+  declare id: CreationOptional<number>; // declara e tipa o id, mas como um parametro opcional
+  declare teamName: string; // declara e tipa teamName como string
+}
+
+// Inicializa o modelo Team com os campos 'id' e 'teamName'
+Team.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+},
+// Configurações do model
+{
+  sequelize: db,
+  modelName: 'team',
+  timestamps: false,
+  underscored: true, // Usa snake_case para os nomes das colunas no banco de dados
+});
+
+
+
+export default Team;
