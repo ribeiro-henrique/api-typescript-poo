19649a5b21647c192c7234c5a835d216794f8c5e
ribeiro-henrique <hesr.ribeiro@gmail.com>
2023-09-28 14:08:01 -0300

    req 9

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -1,11 +1,11 @@
 import * as chai from 'chai';
-// import * as sinon from 'sinon';
+import * as sinon from 'sinon';
 // @ts-ignore
 import chaiHttp = require('chai-http');
 // import { verifyEmail } from '../middlewares/login.verify'
 
 import { app } from '../app';
-// import User from '../database/models/User';
+import User from '../database/models/User';
 
 // import { Response } from 'superagent';
 
@@ -22,6 +22,22 @@ const dado2 = [
     "password": ""
   }
 ]
+
+const dado3 = [
+  {
+    "email": "admin@admin.com",
+    "password": "secret_admin"
+  }
+]
+
+const dado4 = [
+  {
+    "email": "henriqueribeiro@admin.com",
+    "password": "secret_admin"
+  }
+]
+
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjk1OTIwNDYyLCJleHAiOjE2OTYxNzk2NjJ9.TGfQVcTjleGRlT6lBMiVPfP-3oSlFlc4B05YhQUEWlw"
 
 chai.use(chaiHttp);
 
@@ -52,6 +68,32 @@ describe('TDD de Login', function () {
 
     expect(status).to.equal(400);
     expect(body.message).to.deep.equal('All fields must be filled');
+
+  })
+
+  it('should return status 200 and an token', async () => {
+    sinon.stub(User, 'findOne').resolves(token as any)
+
+    const { status, body } = await chai
+    .request(app)
+    .post('/login')
+    .send(dado3);
+
+    expect(status).to.equal(200);
+    expect(body.message).to.deep.equal(token);
+
+  })
+
+  it('should return status 401 if not authorized', async () => {
+    sinon.stub(User, 'findOne').resolves()
+
+    const { status, body } = await chai
+    .request(app)
+    .post('/login')
+    .send(dado4);
+
+    expect(status).to.equal(401);
+    expect(body.message).to.deep.equal({ "message": "Invalid email or password" });
 
   })
 });
